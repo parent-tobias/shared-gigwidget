bc3cc2ce754c27a91fd918374ed38a2858d3e001
tobyPlaysTheUke <parent.tobias@gmail.com>
2026-01-24 18:42:29 -0500

    Fixing redirect error

--- apps/web/src/routes/songs/+page.server.ts
+++ apps/web/src/routes/songs/+page.server.ts
@@ -2,5 +2,5 @@ import { redirect } from '@sveltejs/kit';
 import type { PageServerLoad } from './$types';
 
 export const load: PageServerLoad = async () => {
-  redirect(301, '/library');
+  throw redirect(301, '/library');
 };
